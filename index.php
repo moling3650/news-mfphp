98fbe87963f7b5faf1bf4485e4f1d4276a03e02f
moling <365024424@qq.com>
2016-09-07 00:20:36 +0800

    Enhanced display of debug

--- index.php
+++ index.php
@@ -9,8 +9,14 @@ define('CONFIG_DIR', BASE_DIR . '/core/configs');
 
 define('DEBUG', true);
 
+// 加载第三方库
+include 'vendor/autoload.php';
+
 // 开启或关闭debug模式
 if (DEBUG) {
+    $whoops = new \Whoops\Run;
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+    $whoops->register();
     ini_set('display_error', 'On');
 } else {
     ini_set('display_error', 'Off');
